5cb0cc2ec2bafbbffd12b495cbba1da6dda2d6fe
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-07-08 11:32:33 +0900

    updated our website

---
+++ wepohomepage/src/components/Footer.js
@@ -0,0 +1,17 @@
+
+
+function Footer(){
+    return(
+        <footer id="footer">
+            <div class="footer-text">
+                <a href="#">Termes d'utilisation</a>
+                <a href="#">Code de conduite de l'utilisateur</a>
+                <a href="mailto:wepo.cd@gmail.com"> Email us</a>
+            </div>
+            <p></p>
+            <p >Copyright WEPO 2022</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ wepohomepage/src/components/Hero.js
@@ -0,0 +1,26 @@
+import bg1 from "../bg1.png"
+
+function Hero(){
+    return(
+        <div className="hero-container">
+            {/*<img src={bg1}/>*/}
+            <div className="tagline">
+                <div className="tagline-left">
+
+                    <h1>Lekisa Bor Cho!</h1>
+                    <p>Nous vous aidons à vendre/acheter vos produits électroniques (telephones, télévisions, ordinateurs,etc) en toute securité, du bout de vos doigts.</p>
+                    <ul className="tag-list">
+                        <li>One</li>
+                        <li>One</li>
+                        <li>One</li>
+                    </ul>
+                </div>
+                <div className="tagline-right">
+                    <img id="test"alt="loca"src={bg1}/>
+]                </div>
+
+            </div>
+        </div>
+    )
+}
+export default Hero
