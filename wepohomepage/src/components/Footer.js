73daef87fcf64e326c741ee3b6f184ff66d88300
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-07-18 00:54:15 +0900

    changed the nav bar

--- wepohomepage/src/components/Footer.js
+++ wepohomepage/src/components/Footer.js
@@ -1,8 +1,44 @@
-
+function Signup(){
+    return(
+        <form >
+            <input/>
+            <input/>
+            <button>Submit</button>
+        </form>
+    )
+}
 
 function Footer(){
     return(
         <footer id="footer">
+            <div className="footer-1">
+                <div className="footer-title">
+                    <h3>WEPO RDC</h3>
+                    <div className="footer-sns">
+                        <p>s</p>
+                        <p>s</p>
+
+                        <p>s</p>
+
+                        <p>s</p>
+
+                        <p>s</p>
+
+                    </div>
+                </div>
+                <div className="footer-content">
+                    <div className="footer-about">
+
+                    </div>
+                    <div className="footer-signup">
+                        <h3>Remplissez ce formulaire pour etre le premier a tester notre application</h3>
+                        <Signup/>
+
+                    </div>
+
+                </div>
+
+            </div>
             <div class="footer-text">
                 <a href="#">Termes d'utilisation</a>
                 <a href="#">Code de conduite de l'utilisateur</a>
--- wepohomepage/src/components/Hero.js
+++ wepohomepage/src/components/Hero.js
@@ -1,25 +1,14 @@
-import bg1 from "../bg1.png"
+import bg1 from "../assets/1.png"
 
 function Hero(){
     return(
         <div className="hero-container">
-            {/*<img src={bg1}/>*/}
-            <div className="tagline">
-                <div className="tagline-left">
 
-                    <h1>Lekisa Bor Cho!</h1>
-                    <p>Nous vous aidons à vendre/acheter vos produits électroniques (telephones, télévisions, ordinateurs,etc) en toute securité, du bout de vos doigts.</p>
-                    <ul className="tag-list">
-                        <li>One</li>
-                        <li>One</li>
-                        <li>One</li>
-                    </ul>
-                </div>
-                <div className="tagline-right">
-                    <img id="test"alt="loca"src={bg1}/>
-                </div>
+            <h1>Lekisa Bor Cho!</h1>
+            <p>Nous vous aidons à vendre/acheter vos produits électroniques (telephones, télévisions, ordinateurs,etc) en toute securité, du bout de vos doigts.</p>
+               
 
-            </div>
+
         </div>
     )
 }
