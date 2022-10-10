356d77cb5c2e20cbf89657deff2516ac120a1403
Thisura Thenuka <thenukathisura@gmail.com>
2022-10-10 11:45:00 +0530

    fix(Notification): Added notification after form submission and code refactoring

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-import logo from './weposvg.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 import Form from './components/Form';
-import { useEffect, useState } from 'react';
-
-
-
-
+import {useEffect, useState} from 'react';
 
 
 function App() {
-  const [responsive, setResponsive] =useState("web")
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const [responsive, setResponsive] = useState("web")
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
-  useEffect(()=>{
+    useEffect(() => {
 
-    function watchWidth(){
-      setScreenWidth(window.innerWidth)
-      if (window.innerWidth<=450){
-        setResponsive("mobile")
-        console.log(window.innerWidth)
+        function watchWidth() {
+            setScreenWidth(window.innerWidth)
+            if (window.innerWidth <= 450) {
+                setResponsive("mobile")
+                console.log(window.innerWidth)
 
-      }
-      else if (window.innerWidth>900){
-        setResponsive("web")
-        console.log("Window size is fine")
-      }
-    }
-    window.addEventListener("resize", watchWidth);
-  },[screenWidth])
-  return (
-    <div className="App">
-      <Navbar responsive = {responsive} screenWidth={screenWidth}/>
-      {/*<Form/>*/}
-      
-      <Hero/>
-      <Footer/>
-    </div>
-  );
+            } else if (window.innerWidth > 900) {
+                setResponsive("web")
+                console.log("Window size is fine")
+            }
+        }
+
+        window.addEventListener("resize", watchWidth);
+    }, [screenWidth])
+    return (
+        <div className="App">
+            <Navbar responsive={responsive} screenWidth={screenWidth} />
+            <Form />
+
+            <Hero />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,72 +1,87 @@
-import { useState } from "react"
+import {useState} from "react"
 import logo from "../WEPOLOGOSVG.svg"
 
 
-function Navbar(props){
+function Navbar(props) {
     const [navState, setNavState] = useState(false)
 
-    function toggle(){
+    function toggle() {
         setNavState(prev => !prev)
         console.log(navState)
     }
-    return(
+
+    return (
         <header>
             <nav className="nav">
-                <a href="https://magmukendi.me/wepoHomepage"><img className="wepo-logo"alt="logo-wepo"src={logo}/></a>
+                <a href="https://magmukendi.me/wepoHomepage"><img className="wepo-logo" alt="logo-wepo"
+                                                                  src={logo} /></a>
 
                 {
-                props.screenWidth >600 
-                    ?
-                <div className="navRight">
-                    <div className = "linkContainer">
-                    
-                    <div className="nav-sns">
-                        
-                        <a className="link" href="https://www.instagram.com/wearewepo" target="_blank" >About Us{/*<i className="fa-brands fa-instagram fa-xl"></i>*/}</a>
-                        <a className="link" href="https://www.facebook.com/wearewepo" target="_blank" >Astuces{/*<i className="fa-brands fa-facebook fa-xl"></i>*/}</a>
-                        <a className="link" href="https://wa.me/821027026758" target="_blank" >Contacts{/*<i className="fa-brands fa-whatsapp fa-xl"></i>*/}</a>
-                    </div>
-                
-                    <div id="commission">
-                        <a className="link" href="https://wa.me/821027026758" target="_blank" >Devenez commissionneur</a>
-                    </div>
-                    </div>
-                </div>
-                    :
-                    <a href="#" className="icon" onClick={toggle}>
-    <i className="fa fa-bars"/>
-                </a>
-                    }
+                    props.screenWidth > 600 ?
+                        <div className="navRight">
+                            <div className="linkContainer">
+
+                                <div className="nav-sns">
+
+                                    <a className="link" href="https://www.instagram.com/wearewepo" target="_blank"
+                                       rel="noreferrer">About
+                                        Us{/*<i className="fa-brands fa-instagram fa-xl"></i>*/}</a>
+                                    <a className="link" href="https://www.facebook.com/wearewepo"
+                                       target="_blank"
+                                       rel="noreferrer">Astuces{/*<i className="fa-brands fa-facebook fa-xl"></i>*/}</a>
+                                    <a className="link" href="https://wa.me/821027026758"
+                                       target="_blank"
+                                       rel="noreferrer">Contacts{/*<i className="fa-brands fa-whatsapp fa-xl"></i>*/}</a>
+                                </div>
+
+                                <div id="commission">
+                                    <a className="link" href="https://wa.me/821027026758" target="_blank"
+                                       rel="noreferrer">Devenez
+                                        commissionneur</a>
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        <a href="#" className="icon" onClick={toggle}>
+                            <i className="fa fa-bars" />
+                        </a>
+                }
             </nav>
             {
                 navState &&
 
-                <div style={{height:window.innerHeight}} className="responsiveNav">
-                        <button id="close" onClick={toggle}>Close</button>
+                <div style={{height: window.innerHeight}} className="responsiveNav">
+                    <button id="close" onClick={toggle}>Close</button>
                     <div className="responsiveEl">
 
-            <a className="link" href="https://www.instagram.com/wearewepo" target="_blank" >About Us{/*<i className="fa-brands fa-instagram fa-xl"></i>*/}</a>
-                        <a className="link" href="https://www.facebook.com/wearewepo" target="_blank" >Astuces{/*<i className="fa-brands fa-facebook fa-xl"></i>*/}</a>
-                        <a className="link" href="https://wa.me/821027026758" target="_blank" >Contacts{/*<i className="fa-brands fa-whatsapp fa-xl"></i>*/}</a>
+                        <a className="link" href="https://www.instagram.com/wearewepo" target="_blank" rel="noreferrer">About
+                            Us{/*<i className="fa-brands fa-instagram fa-xl"></i>*/}</a>
+                        <a className="link" href="https://www.facebook.com/wearewepo"
+                           target="_blank"
+                           rel="noreferrer">Astuces{/*<i className="fa-brands fa-facebook fa-xl"></i>*/}</a>
+                        <a className="link" href="https://wa.me/821027026758"
+                           target="_blank"
+                           rel="noreferrer">Contacts{/*<i className="fa-brands fa-whatsapp fa-xl"></i>*/}</a>
                     </div>
                     <div className="sns-logo">
 
-                        <a href="https://www.instagram.com/wearewepo" target="_blank" >
+                        <a href="https://www.instagram.com/wearewepo" target="_blank" rel="noreferrer">
 <span>
 
-                            <i className="fa-brands fa-instagram fa-xl"/>
+                            <i className="fa-brands fa-instagram fa-xl" />
 </span>
-                            </a>
-                        <a href="https://www.facebook.com/wearewepo" target="_blank" >
-                            <i className="fa-brands fa-facebook fa-xl"/>
                         </a>
-                        <a href="https://wa.me/821027026758" target="_blank" >
-                            <i className="fa-brands fa-whatsapp fa-xl"/>
+                        <a href="https://www.facebook.com/wearewepo" target="_blank" rel="noreferrer">
+                            <i className="fa-brands fa-facebook fa-xl" />
                         </a>
-                        </div>
-            </div>
+                        <a href="https://wa.me/821027026758" target="_blank" rel="noreferrer">
+                            <i className="fa-brands fa-whatsapp fa-xl" />
+                        </a>
+                    </div>
+                </div>
             }
         </header>
     )
 }
+
 export default Navbar
